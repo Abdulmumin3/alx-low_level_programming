281c06b8464483c330c751ae4028dacfd0975953
abdulmumeen <abdulmumeenadesoye19@gmail.com>
2022-09-30 13:29:25 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -66,4 +66,20 @@ int main(int argc, char *argv[])
 			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
 				puts("Error");
+				return (1);
+			}
+		}
+	}
 
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
